6e8fb3e8e1272e8967c3f2cb89a666e1e7c243e5
Сулейман Хасанов <macbook@MacBook-Air.local>
2024-03-23 22:41:31 +0500

    add skeleton for news

---
+++ src/components/Skeleton/Skeleton.jsx
@@ -0,0 +1,19 @@
+import styles from "./Skeleton.module.css";
+
+const Skeleton = ({ count, type }) => {
+  return (
+    <>
+      {count > 1 ? (
+        <ul className={styles.list}>
+          {[...Array(count)].map((_, index) => (
+            <li key={index} className={type === "banner" ? styles.banner : styles.item} />
+          ))}
+        </ul>
+      ) : (
+        <li className={type === "banner" ? styles.banner : styles.item} />
+      )}
+    </>
+  );
+};
+
+export default Skeleton;
